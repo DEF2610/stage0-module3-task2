c6b232f984867b0355e694f97a388c9a7757db4f
Temka <maksimukart@gmail.com>
2023-01-22 16:24:52 +0100

    done

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,13 +2,14 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable_1 = 0;
-        int variable_2 = 2000;
-        int value_1 = 1000;
-        int variable_3 = 4000;
-        int value2 = 3000;
-        int variables_1 = 5000;
-        int variables_2 = 7000;
-        int value_2 = 90000;
+
+        int variableOne = 0;
+        int variableTwo = 2000;
+        int valueOne = 1000;
+        int variableThree = 4000;
+        int valueTwo = 3000;
+        int variablesOne = 5000;
+        int variablesTwo = 7000;
+        int valueThree = 90000;
     }
 }
--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -2,6 +2,7 @@ package lang.print.gaps.task2;
 
 public class JavaVariableNames {
     public static void main(String[] args) {
+
         int morrow = 0;
         int money = 10;
         int hundred = 100;
@@ -9,6 +10,6 @@ public class JavaVariableNames {
         int underscore = 10000;
         int sharp = 100000;
         int star = 1000000;
-        int bad_example_of_var_name = 1000000;
+        int badExampleOfVarName = 1000000;
     }
 }
